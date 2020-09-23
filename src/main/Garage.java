e61f25fff6cfc48949283620acde0a3800f359ac
Lewis Thomson <lewisfalconer@outlook.com>
2020-09-23 17:37:01 +0100

    Allowed the main function to call the garage

--- src/main/Garage.java
+++ src/main/Garage.java
@@ -14,49 +14,52 @@ public class Garage {
 	public Garage() {
 		falconGarage = new ArrayList<Vehicle>();
 		
-		//adding car
-		addVehicle("Lewis", "Diesel", 4, "F41C0N", 5, false);
-		
-		//adding boat
-		addVehicle("McBoatface", "Petrol", 30.0f, 6, true);
+	}
 	
-		//adding two motorcylces
-		addVehicle("Michal", "Petrol", 2, "S14V", 11.0f);
-		addVehicle("Lefkos", "Diesel", 2, "C9R107", 9.0f);
-		
-		fixVehicles();
+	public void runGarage() {
+		//adding car
+				addVehicle("Lewis", "Diesel", 4, "F41C0N", 5, false);
+				
+				//adding boat
+				addVehicle("McBoatface", "Petrol", 30.0f, 6, true);
+			
+				//adding two motorcylces
+				addVehicle("Michal", "Petrol", 2, "S14V", 11.0f);
+				addVehicle("Lefkos", "Diesel", 2, "C9R107", 9.0f);
+				
+				fixVehicles();
 	}
 
 	//For Cars
-	public void addVehicle(String owner, String fuelType, int wheels, String reg, int doors, Boolean isManual) {
+	private void addVehicle(String owner, String fuelType, int wheels, String reg, int doors, Boolean isManual) {
 		falconGarage.add(new Car(falconGarage.size(), owner, fuelType, wheels, reg, doors, isManual ));
 	}
 	
 	//For Motorcycles
-	public void addVehicle(String owner, String fuelType, int wheels, String reg, float tyreSize) {
+	private void addVehicle(String owner, String fuelType, int wheels, String reg, float tyreSize) {
 		falconGarage.add(new Motorcycle(falconGarage.size(), owner, fuelType, wheels, reg, tyreSize));
 	}
 
 	//For Boats
-	public void addVehicle(String owner, String fuelType, float len, float wid, Boolean glitter) {
+	private void addVehicle(String owner, String fuelType, float len, float wid, Boolean glitter) {
 		falconGarage.add(new Boat(falconGarage.size(), owner, fuelType, 0, len, wid, glitter));
 	}
 	
-	public void removeVehicle(String vehicleType) {
+	private void removeVehicle(String vehicleType) {
 		
 	}
 	
-	public void removeVehicle(int ID) {
+	private void removeVehicle(int ID) {
 		
 	}
 	
-	public void fixVehicles(){
+	private void fixVehicles(){
 		for(Vehicle index : falconGarage){
 			index.fixVehicle();
 		}
 	}
 	
-	public void emptyGarage() {
+	private void emptyGarage() {
 		falconGarage.clear();
 	}
 }
